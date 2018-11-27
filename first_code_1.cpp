632a92551df91d6c5545f57a8ca34cbe65fdcb32
Josip7171 <44698188+Josip7171@users.noreply.github.com>
2018-11-27 09:44:55 +0100

    Test second branch
    Testiranje second brancha, savea odvojeno od mastera

--- first_code_1.cpp
+++ first_code_1.cpp
@@ -4,7 +4,7 @@
 
 int main(){									// osnovna funkcija, od nje sve krece
 	std::cout<<"hi world!" << std::endl;	// cout = Console OUTput, naredba za ispis teksta
-	std::cout<< 1 << std::endl;
+	std::cout<< 1 << std::endl;				//Novi komentar za test za GitHub
 	system("PAUSE");						// naredba koja pauzira program da se vidi rezultat
 	return 0;								// naredba koja vraca vrijednost (INT u ovom slucaju jer je funkcija INT tip)
 }
