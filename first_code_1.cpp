0d474bbe8e8054d32c2682132f1ce53b877abae7
Josip7171 <44698188+Josip7171@users.noreply.github.com>
2018-11-27 11:11:48 +0100

    Test git iz cmd-a

--- first_code_1.cpp
+++ first_code_1.cpp
@@ -4,7 +4,7 @@
 
 int main(){									// osnovna funkcija, od nje sve krece
 	std::cout<<"hi world!" << std::endl;	// cout = Console OUTput, naredba za ispis teksta
-	std::cout<< 1 << std::endl;				//Novi komentar za test za GitHub
+	std::cout<< 1 << std::endl;				//Novi komentar za test za GitHub , novi komentar
 	system("PAUSE");						// naredba koja pauzira program da se vidi rezultat
 	return 0;								// naredba koja vraca vrijednost (INT u ovom slucaju jer je funkcija INT tip)
 }
