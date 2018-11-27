bcc635b67b476edb425cfde505f9523f829e8deb
Josip7171 <44698188+Josip7171@users.noreply.github.com>
2018-11-27 09:28:18 +0100

    Hi world!
    First commit to Master code "Hi world!"

---
+++ first_code_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>							// ukljucivanje osnovnih naredaba?
+
+
+
+int main(){									// osnovna funkcija, od nje sve krece
+	std::cout<<"hi world!" << std::endl;	// cout = Console OUTput, naredba za ispis teksta
+	std::cout<<123 << std::endl;
+	system("PAUSE");						// naredba koja pauzira program da se vidi rezultat
+	return 0;								// naredba koja vraca vrijednost (INT u ovom slucaju jer je funkcija INT tip)
+}
+
+
